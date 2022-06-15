f14a72a277a789196cddf7ec2149ae89abbc1260
Gianfranco Pedrol <gianfrancosites@gmail.com>
2022-06-15 15:41:52 -0300

    ajuste seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UserSeeder::class);
-        //$this->call(PermissoesSeeder::class);
-        //$this->call(LogsSeeder::class);
-        //$this->call(LogsUserExames::class);
-        //$this->call(DepartmentSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(PermissoesSeeder::class);
+        $this->call(LogsSeeder::class);
+        $this->call(LogsUserExames::class);
+        $this->call(DepartmentSeeder::class);
         $this->call(DomainsSeeder::class);
     }
 }

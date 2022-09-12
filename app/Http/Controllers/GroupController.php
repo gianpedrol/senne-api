674626bd4582d3dda4c75c1d9f92e5f9cb3d43b3
jornadadocliente <evelise@jornadadocliente.com.br>
2022-09-12 18:13:11 -0300

    ajuste lista

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -148,15 +148,13 @@ class GroupController extends Controller
 
         /* CASO NÃO TENHA NENHUM GRUPO CADASTRADO NO BANCO ELE IRÁ CRIAR*/
         foreach ($data as $name) {
-           /* $groupCheck =  Groups::where('name',  $name['name'])->first();
+            $groupCheck =  Groups::where('name',  $name['name'])->first();
             // dd(  $groupCheck);
             if($groupCheck){    
-                $groupCheck->update(['name' => $name['name'], 'codgroup' =>  $name['codgrupo'] ]);
+                $groupCheck->update(['codgroup' =>  $name['codgrupo']]);
             }else{
                 Groups::create([ 'name' => $name['name'], 'codgroup',  $name['codgrupo']]);
-            }*/
-          //  dd($name['codgrupo']);
-          Groups::create([ 'name' => $name['name'], 'codgroup',  $name['codgrupo']]);
+            }
 
         }
         /* LISTA TODOS OS GRUPOS APÓS CONSULTA E SALVAR NOVOS DADOS  */
